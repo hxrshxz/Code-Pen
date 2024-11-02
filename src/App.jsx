ff937dd4d5ad42b90307b2118ce987faf5d7f24d
Harsh <harshmastic@gmail.com>
2024-11-02 22:59:47 +0530

    commented maps iframe

--- src/App.jsx
+++ src/App.jsx
@@ -67,14 +67,14 @@ const App = () => {
           language="javascript"
         />
       )}
-      <iframe
+      {/* <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.835434509985!2d144.9556517151313!3d-37.81628274207198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577c8e4f24a8b8d!2sFlinders%20Street%20Station!5e0!3m2!1sen!2sau!4v1636193545209!5m2!1sen!2sau"
         width="600"
         height="450"
        style={{border:0}}
         allowfullscreen=""
         loading="lazy"
-      ></iframe>
+      ></iframe> */}
 
 
     </div>
